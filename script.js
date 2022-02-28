957f6107fdbf62d336782ed8b5e99a4f34b622fd
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-02-28 14:30:32 -0300

    Requisito 14 finalizado, function 14 apagado, radio input em  html

--- script.js
+++ script.js
@@ -11,19 +11,5 @@ function clickLogin(enterButton) {
     alert('Email ou senha inválidos.');
   }
 }
-// Função para gerar radio buttons (optei por essa forma para não precisar colocar 1 linha a mais no html para cada botão), *precisa de ajustes*
 
-// console.log(document.getElementById('label-rate').innerText)
-// function generateRateButton() {
-//   for (let index = 1; index <= 10; index += 1) {
-//     const rateButton = document.createElement('input');
-//     rateButton.type = 'radio';
-//     rateButton.name = 'rate-button';
-//     rateButton.value = index;
-//     rateButton.innerText = index;
-//     document.getElementById('label-rate').appendChild(rateButton);
-//   }
-// }
-
-// generateRateButton();
 button.addEventListener('click', clickLogin);
