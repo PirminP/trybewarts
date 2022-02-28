cad5ce5d5e16bc1a95e2bbddb918b3f8df584ff6
José Augusto <jaaugusto.n@gmail.com>
2022-02-28 12:41:15 -0300

    Adiciona seção de comentário, botão de submeter formulário, checkbox para aceitar termos e seção para escolha de família e conteúdo

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+const button = document.querySelector('#enter-button');
+
 function clickLogin(enterButton) {
-  const email = document.getElementById('email');
-  const password = document.getElementById('password');
+  enterButton.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
   console.log(email, password);
-  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+  if (email === 'tryber@teste.com' && password === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
-  return enterButton;
 }
+// console.log(document.getElementById('label-rate').innerText)
+// function generateRateButton() {
+//   for (let index = 1; index <= 10; index += 1) {
+//     const rateButton = document.createElement('input');
+//     rateButton.type = 'radio';
+//     rateButton.name = 'rate-button';
+//     rateButton.value = index;
+//     rateButton.innerText = index;
+//     document.getElementById('label-rate').appendChild(rateButton);
+//   }
+// }
 
-const button = document.getElementById('form-login');
-button.addEventListener('click', clickLogin());
+// generateRateButton();
+button.addEventListener('click', clickLogin);
